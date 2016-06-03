b533d6972cba3ed45dc8c774cc0e437a9a5205cc
Gordan <me@ggordan.com>
2016-06-03 14:45:41 +0100

    wip

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 const app = express();
-const urlParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json()
 
-app.listen(process.env.PORT || 8080, () => console.log('Starting server on 8080'));
+app.listen(process.env.PORT || 8090, () => console.log('Starting server on 8090'));
+const formatMessage = (msg) => `data: ${msg}\n\n`;
 
-app.post('/session', cors(), urlParser, (req, res) => {
+app.post('/session', cors(), jsonParser, (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
   console.log(req.body);
-  console.log('pinged me');
+  // res.write(formatMessage(JSON.stringify(req.body)));
   res.end('ok');
 });
